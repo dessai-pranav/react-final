d44b35caa9e471468820d3fe8d88dc945e44e759
pranav <kishrgdesai@gmail.com>
2025-10-17 20:45:27 +0530

    static part

--- eat_n_slice/bill_split/src/App.jsx
+++ eat_n_slice/bill_split/src/App.jsx
@@ -23,7 +23,10 @@ const initialFriends = [
   return <div className='app'>
     <div className='sidebar'>
     <FriendsList/>
+    <FormAddFriend/>
+     <Button>Add Friend</Button>
     </div>
+    <FormSplitBill/>
   </div>
  } 
  function FriendsList(){
@@ -40,7 +43,40 @@ const initialFriends = [
     {friend.balance < 0 && <p className="red">You Owe {friend.name} ${Math.abs(friend.balance)}</p>}
     {friend.balance > 0 && <p className="green">{friend.name} Owes you ${Math.abs(friend.balance)}</p>}
 {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-<button className='button'>Select</button>
 
+<Button>Select</Button>
   </li>
+ }
+
+ function Button({children}){
+  return <button className='button'>{children}</button>
+ }
+ function FormAddFriend(){
+  return <form className='form-add-friend'>
+    <label>Friend Name</label>
+    <input type = "text" />
+    <label> Image URL</label>
+     <input type = "text" />
+     <Button>Add</Button>
+
+  </form>
+ }
+
+ function FormSplitBill(){
+  return <form className='form-split-bill'>
+    <h2>Split a bill with X </h2>
+    <label> Bill Value</label>
+     <input type = "text" />
+     <label>Your Expense</label>
+     <input type = "text" />
+     <label> X's expense</label>
+     <input type = "text" disabled />
+     <label> Who is Paying The Bill
+      <select>
+        <option value ='user'>You</option>
+        <option value ='friend'>X</option>
+      </select>
+     </label>
+     <Button>Split bill</Button>
+  </form>
  }
