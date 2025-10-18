cd79bdff7ab362b12f18e0a92f68065fb76cf323
pranav <kishrgdesai@gmail.com>
2025-10-18 21:31:39 +0530

    handling error usepopcorn

--- usePopcorn/usepopcorn/src/main.jsx
+++ usePopcorn/usepopcorn/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-{/*import './index.css'
-import App from './App.jsx'*/}
+import './index.css'
+import App from './App1.jsx'
 import StarRating from './StarRating'
 
-function Test(){
+{/*function Test(){
   const [movieRating,setMovieRating] = useState(0);
   return(
     <div>
@@ -13,11 +13,11 @@ function Test(){
     </div>
 
   )
-}
+}*/}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   {/* <App />*/}
-<StarRating maxRating={5} color="red" classname="test" messages={["terrible","bad","okay","good","Amazing"]} defaultRating={3}/>
-<Test/>
+    <App />
+{/*<StarRating maxRating={5} color="red" classname="test" messages={["terrible","bad","okay","good","Amazing"]} defaultRating={3}/>
+<Test/>*/}
   </StrictMode>,
 )
