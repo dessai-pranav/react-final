cf26f532cabe098fbda14557471c04e6e03c4d8b
pranav <kishrgdesai@gmail.com>
2025-10-24 15:34:23 +0530

    sidebar and map

---
+++ worldwise/worldwise/src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import AppNav from "./AppNav"
+import Logo from "./Logo"
+import styles from "./Sidebar.module.css"
+function Sidebar() {
+    return (
+        <div className={styles.sidebar}>
+            <Logo/>
+            <AppNav/>
+            <p> List Of Cities</p>
+            <footer className = {styles.footer}>
+                <p className={StyleSheet.copyright}>
+                    &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+                </p>
+            </footer>
+        </div>
+    )
+}
+
+export default Sidebar
