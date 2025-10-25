6950a5e8c8dbf231263b5fe7f855a48d76887bce
pranav <kishrgdesai@gmail.com>
2025-10-25 06:37:41 +0530

    context api

--- atomic-blog/src/App-v1.js
+++ atomic-blog/src/App-v1.js
@@ -16,6 +16,7 @@ function App() {
     Array.from({ length: 30 }, () => createRandomPost())
   );
   const [searchQuery, setSearchQuery] = useState("");
+  const [isFakeDark, setIsFakeDark] = useState(false);
 
   // Derived state. These are the posts that will actually be displayed
   const searchedPosts =
