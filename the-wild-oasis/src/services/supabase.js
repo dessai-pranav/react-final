0f8e9bb9da8487304b90853b336791dd7e94918f
pranavdessaii <kishrgdesai@gmail.com>
2025-11-22 14:33:30 +0530

    connecting supabase

---
+++ the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://wcedrsnpfqbrpycdxzjy.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjZWRyc25wZnFicnB5Y2R4emp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3OTU1OTYsImV4cCI6MjA3OTM3MTU5Nn0.yCnnmrBJwoIZOuuiOXR0qHDe-b9teJlyUOd1U3ZCRYQ"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
